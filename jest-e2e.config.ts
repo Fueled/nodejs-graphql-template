5761001f3ac77eb1b2810b40dad5ee75ba192d9e
Kaspars Zarinovs <k.zarinovs@gmail.com>
2022-06-02 09:15:28 +0300

    Cleaned up jest config, added e2e config

---
+++ jest-e2e.config.ts
@@ -0,0 +1,10 @@
+export default {
+  "moduleFileExtensions": ["js", "json", "ts"],
+  "modulePaths": ["<rootDir>"],
+  "rootDir": "./",
+  "testEnvironment": "node",
+  "testRegex": ".e2e-spec.ts$",
+  "transform": {
+    "^.+\\.(t|j)s$": "ts-jest"
+  }
+};
