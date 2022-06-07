b9f108683894d5f6349f80e99edd75fd7a8a2716
Kaspars Zarinovs <k.zarinovs@gmail.com>
2022-06-07 14:36:15 +0300

    Updated core app structure - simpliefied.

--- src/graphql.ts
+++ src/graphql.ts
@@ -1,12 +1,19 @@
-import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
+import {
+  ApolloServerPluginDrainHttpServer,
+  ApolloServerPluginLandingPageGraphQLPlayground,
+} from "apollo-server-core";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
 import { readFileSync } from "fs";
+import http from "http";
 import config from "./config/config";
 import context from "./context";
 import resolvers from "./resolvers";
 
-export default async (app: express.Application): Promise<void> => {
+export default async function startGraphQLServer(
+  app: express.Application,
+  httpServer: http.Server
+): Promise<void> {
   const typeDefs = readFileSync(`${__dirname}/schema.graphql`).toString("utf-8");
 
   const server = new ApolloServer({
@@ -15,12 +22,13 @@ export default async (app: express.Application): Promise<void> => {
     context,
     plugins: [
       ApolloServerPluginLandingPageGraphQLPlayground(),
-      // TODO: May need drain HTTP server plugin
+      ApolloServerPluginDrainHttpServer({ httpServer }),
     ],
+    csrfPrevention: config.graphql.csrfPrevention,
     introspection: config.graphql.introspectionEnabled,
   });
 
   await server.start();
 
   server.applyMiddleware({ app });
-};
+}
---
+++ src/index.ts
@@ -0,0 +1,29 @@
+import errorHandler from "errorhandler";
+import express from "express";
+import http from "http";
+import config from "./config/config";
+import startGraphQLServer from "./graphql";
+
+async function main(): Promise<void> {
+  // Create Express server
+  const app: express.Application = express();
+  const httpServer = http.createServer(app);
+
+  //Error Handler. Provides full stack trace.
+  if (config.debug) {
+    app.use(errorHandler());
+  }
+
+  await startGraphQLServer(app, httpServer);
+
+  // Start Express server.
+  await new Promise<void>((resolve) => httpServer.listen(config.http.port, resolve));
+  console.log(
+    "[Server] Server is running on " +
+      `${config.http.schema}://${config.http.host}:${config.http.port}` +
+      ` in ${app.get("env") as string} mode.`
+  );
+  console.log("[Server] Press CTRL-C to stop.\n");
+}
+
+void main();
