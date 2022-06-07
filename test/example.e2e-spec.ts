8854d42225eb887948423d311af9188116c34b9e
Kaspars Zarinovs <k.zarinovs@gmail.com>
2022-06-07 15:26:30 +0300

    Updated E2E tests with graphql. Updated package scripts.

--- test/example.e2e-spec.ts
+++ test/example.e2e-spec.ts
@@ -1,7 +1,10 @@
+import { gql } from "apollo-server-core";
+import { ApolloServer } from "apollo-server-express";
 import express from "express";
+import http from "http";
 import request from "supertest";
 
-describe("a test with a HTTP request", () => {
+describe("a REST based HTTP request", () => {
   const app = express();
 
   app.get("/hello", function (req, res) {
@@ -9,7 +12,7 @@ describe("a test with a HTTP request", () => {
   });
 
   describe("GET /hello", () => {
-    it("should return a successful JSON response", async () => {
+    it("should return a successful response", async () => {
       return new Promise((done) => {
         request(app)
           .get("/hello")
@@ -20,5 +23,59 @@ describe("a test with a HTTP request", () => {
           .expect(200, done);
       });
     });
+  });
+});
+
+describe("a GraphQL based HTTP request", () => {
+  let app: express.Application;
+  let httpServer: http.Server;
+  let server: ApolloServer;
+
+  const typeDefs = gql`
+    type Query {
+      hello(name: String): String!
+    }
+  `;
+
+  const resolvers = {
+    Query: {
+      // eslint-disable-next-line
+      hello: (_: any, { name }: { name: string }) => `Hello ${name}!`,
+    },
+  };
+
+  describe("POST /graphql", () => {
+    beforeAll(async () => {
+      app = express();
+      httpServer = http.createServer(app);
+      server = new ApolloServer({
+        typeDefs,
+        resolvers,
+      });
+      await server.start();
+      server.applyMiddleware({ app });
+    });
+
+    afterAll(async () => {
+      await server.stop();
+    });
+
+    it("should return a successful response", async () => {
+      const queryData = {
+        query: `query sayHello($name: String) {
+          hello(name: $name)
+        }`,
+        variables: { name: 'world' },
+      };
+
+      const response = await request(httpServer)
+        .post("/graphql")
+        .send(queryData);
+
+      expect(response.statusCode).toBe(200);
+      expect(response.body.data).toEqual({
+        hello: 'Hello world!',
+      });
+  });
   });
 });
